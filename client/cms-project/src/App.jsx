2a8bff4a4930fcfdd979a3e09903a8488609edb0
Parivel2004 <parivelravi2004@gmail.com>
2025-05-28 09:46:25 +0000

    fish

--- client/cms-project/src/App.jsx
+++ client/cms-project/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import React from 'react'
 import './App.css'
 import About from './pages/About'
 
 function App() {
-const [count, setCount] = useState(0);
   return (
     <>
       <About/>
