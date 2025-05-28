5b4cc5a16565f10092bee67379d324ec0c5bf213
Parivel2004 <parivelravi2004@gmail.com>
2025-05-28 06:39:01 +0000

    ok

--- client/cms-project/vite.config.js
+++ client/cms-project/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server:{
+    allowedHosts: ["5173-priyan-2004-cms-project-vlc0xj077f.app.codeanywhere.com"],
+  },
+  plugins: [react(),tailwindcss()],
+  
 })
