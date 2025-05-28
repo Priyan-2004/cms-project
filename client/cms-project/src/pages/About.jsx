00583aebb78b4a143cf6a69fdd08c733ef62a1d8
Parivel2004 <parivelravi2004@gmail.com>
2025-05-28 09:19:07 +0000

    ok bro

--- client/cms-project/src/pages/About.jsx
+++ client/cms-project/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <h1>About</h1>
+            <h1 onClick={About}About></h1>
             <p>This is the about pages</p>
             </div>
     )
