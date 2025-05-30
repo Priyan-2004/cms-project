d37038058d7364e928a21060c1ad536223e47f72
Parivel2004 <parivelravi2004@gmail.com>
2025-05-30 09:09:24 +0000

    come

--- client/cms-project/src/App.jsx
+++ client/cms-project/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import './App.css'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
+    <Home/>
     <SignUp/>
     <About/>
+   
 
     </>
   )
